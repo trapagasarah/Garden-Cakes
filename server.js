ba5df7f71e713615af645dff97cf5a9978121744
Sarah Trapaga <trapagasarah@gmail.com>
2019-04-25 11:30:45 -0400

    created server file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const customerRoutes = require('./routes/customerRoutes')
+
+
+
+
+
+
+const express = require('express');
+const app = express()
+const methodOverride = require('method-override')
+const customerRoutes = require('./routes/customerRoutes')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'hbs')
+
+app.use(express.static(__dirname + '/public'))
+
+app.use('/', customerRoutes)
+
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('Connected at: ' + PORT)
+})
