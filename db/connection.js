a8cfe6d50b326862d8dfe8971e05545227b574a1
Sarah Trapaga <trapagasarah@gmail.com>
2019-04-25 13:07:12 -0400

    Updated DB connection code to use MongoLab when in Production

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 const connectionString = 'mongodb://localhost/Garden-Cakes';
-mongoose.connect(connectionString, {useNewUrlParser: true})
-    .then(() => {
-        console.log('Connected to MongoDB');
-    });
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect(connectionString)
+}
+mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -3,29 +3,18 @@ const app = express();
 const methodOverride = require('method-override');
 const customerRoutes = require('./routes/customerRoutes')
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
+app.set('view engine', 'hbs');
 
+app.use(express.static(__dirname + '/public'));
+app.use('/', customerRoutes);
 
-
-
-const express = require('express');
-const app = express()
-const methodOverride = require('method-override')
-const customerRoutes = require('./routes/customerRoutes')
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(methodOverride('_method'))
-
-app.set('view engine', 'hbs')
-
-app.use(express.static(__dirname + '/public'))
-
-app.use('/', customerRoutes)
-
-
-
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Connected at: ' + PORT)
 })
+
+
